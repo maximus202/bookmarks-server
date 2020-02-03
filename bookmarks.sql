b6d0ff52cebb900fdb17d90b14ad4dd89280507f
Max R <maxx.ratto@gmail.com>
2020-02-03 13:27:13 -0700

    Added bookmarks script

---
+++ bookmarks.sql
@@ -0,0 +1,12 @@
+-- Insert test data
+insert into bookmarks (url)
+values
+('www.facebook.com'),
+('www.youtube.com'),
+('www.twitter.com'),
+('www.pandora.com'),
+('www.uccu.com'),
+('reddit.com'),
+('www.a24films.com'),
+('www.yes.com'),
+('www.google.com');
